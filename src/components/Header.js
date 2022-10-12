c22b286a2b22e7b655a0dc37266dc5c5ad0d8884
makbarm <makbarm.id@gmail.com>
2022-10-12 18:36:37 +0700

    move button addnote, logout to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,34 @@
 import React from "react";
+import { FiFilePlus, FiLogOut } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import Button from "./Button";
 
 const Header = (props) => {
 	return (
-        <header className="Header">
-            <div className="Searchbar">
-                <input
-                type="text"
-                placeholder="Search notes..."
-                value={props.keyword}
-                onChange={(e) => props.keywordChange(e.target.value)}
-                />
-            </div>
-            <div className="toggle">
-                <p className="lang">EN</p>
-                <p className="logut">Logout</p>
-            </div>
-        </header>
-    )
+		<header className="Header">
+			<div className="Searchbar">
+				<input
+					type="text"
+					placeholder="Search notes..."
+					value={props.keyword}
+					onChange={(e) => props.keywordChange(e.target.value)}
+				/>
+			</div>
+			<div className="toggle">
+				<Link to="/add">
+					<Button className="btn-addpage" label={<FiFilePlus />} />
+				</Link>
+				<p className="lang">EN</p>
+				<Link>
+					<Button
+						className="btn-logout"
+						onClick={props.onLogout}
+						label={<FiLogOut />}
+					/>
+				</Link>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,8 @@
 import React from "react";
-import { FiLogOut, FiFilePlus } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 const Navigation = (props) => {
-	return (
-		<div className="Navigation">
-			<li>
-				<button type="button" onClick={props.onLogout}>
-					<FiLogOut />
-				</button>
-				<br />
-				<Link to="/add"><button><FiFilePlus /></button></Link>
-				
-			</li>
-		</div>
-	);
+	return <div className="Navigation"></div>;
 };
 
 export default Navigation;
