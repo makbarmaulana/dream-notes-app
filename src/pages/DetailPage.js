faedb2f0369cb7222de9b751dd987ecbff2eff79
makbarm <makbarm.id@gmail.com>
2022-10-22 04:07:38 +0700

    ADD : loading animation to Detail Page

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -7,6 +7,7 @@ import Button from "../components/Action/Button";
 const DetailPage = () => {
 	const { id } = useParams();
 	const [note, setNote] = React.useState(null);
+	const [loading, setLoading] = React.useState(true);
 
 	React.useEffect(() => {
 		getNoteData(id);
@@ -15,24 +16,27 @@ const DetailPage = () => {
 	const getNoteData = async (id) => {
 		getNote(id).then(({ data }) => {
 			setNote(data);
+			setLoading(false);
 		});
 	};
 
 	return (
 		<div className="DetailPage">
-			<div className="NoteDetail">
-				<div className="note-detail-content">
-					<p className="createdAt">
-						{ShowFormattedDate(note?.createdAt)}
-					</p>
-					<h3 className="title">{note?.title}</h3>
-					<p className="body">{note?.body}</p>
+			{loading ? (
+				<div className="loader " />
+			) : (
+				<div className="NoteDetail">
+					<div className="note-detail-content">
+						<p className="createdAt">{ShowFormattedDate(note?.createdAt)}</p>
+						<h3 className="title">{note?.title}</h3>
+						<p className="body">{note?.body}</p>
+					</div>
+					<div className="buttons">
+						<Button className="btn-archive" />
+						<Button className="btn-delete" />
+					</div>
 				</div>
-				<div className="buttons">
-					<Button className="btn-archive" />
-					<Button className="btn-delete" />
-				</div>
-			</div>
+			)}
 		</div>
 	);
 };
